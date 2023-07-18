aba6abf07de0eb5a9cabd6b86f254b0f7ecc0e2d
Chiamaka Osuji <hello.chiamaka@gmail.com>
2023-07-18 02:17:21 -0700

    added filter functionality for company waitlist

--- admin/hiring.js
+++ admin/hiring.js
@@ -2,54 +2,33 @@
 const companies = [
     {
         "id": 1,
-        "companyName": "Google",
+        "companyName": "Gitstart",
         "description": "Google Inc Development company",
         "industry": "Information Technology",
-        "dateCreated" : "2015",
-        "action": [ `<div>
-        <button>View</button>
-    
-        <button>Approve</button>
-        </div>`
-    ]
+        "dateCreated" : "2015"
+     
 },
 {
     "id": 2,
-    "companyName": "Google",
+    "companyName": "Amazon",
     "description": "Google Inc Development company",
     "industry": "Information Technology",
-    "dateCreated" : "2015",
-    "action": [ `<div>
-    <button>View</button>
+    "dateCreated" : "2015"
 
-    <button>Approve</button>
-    </div>`
-]
 },
 {
     "id": 3,
-    "companyName": "Google",
+    "companyName": "Facebook",
     "description": "Google Inc Development company",
     "industry": "Information Technology",
-    "dateCreated" : "2015",
-    "action": [ `<div>
-    <button>View</button>
-    <button>Approve</button>
-    </div>`
-]
+    "dateCreated" : "2015"
 },
 {
     "id": 4,
-    "companyName": "Google",
+    "companyName": "Twitter",
     "description": "Google Inc Development company",
     "industry": "Information Technology",
-    "dateCreated" : "2015",
-    "action": [ `<div sty>
-    <button>View</button>
-
-    <button>Approve</button>
-    </div>`
-]
+    "dateCreated" : "2015"
 }
 ];
 
